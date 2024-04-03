0ac67014c8ef5bd92f0671921125ffcbfae8c7d8
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-04-03 10:06:40 +0200

    feat(table-transformer.py-requirements.txt): got table detection working

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bank-statement-pdf-to-csv
+
+layout analysis:
+
+```bash
+$ surya_layout /path/to/my.pdf --images
+```
---
+++ experiments/table-transformer.py
@@ -0,0 +1,41 @@
+import torch
+from transformers import AutoImageProcessor, TableTransformerForObjectDetection
+from PIL import Image
+
+# file_path = hf_hub_download(repo_id="nielsr/example-pdf", repo_type="dataset", filename="example_pdf.png")
+file_path = "/Users/josephbolton/Downloads/bank_statement_images/output-1.png"
+image = Image.open(file_path).convert("RGB")
+
+image_processor = AutoImageProcessor.from_pretrained(
+    "microsoft/table-transformer-detection"
+)
+print("loaded image processor")
+model = TableTransformerForObjectDetection.from_pretrained(
+    "microsoft/table-transformer-detection"
+)
+print("loaded model")
+
+# prepare image for the model
+inputs = image_processor(images=image, return_tensors="pt")
+
+# forward pass
+outputs = model(**inputs)
+
+# convert outputs (bounding boxes and class logits) to Pascal VOC format (xmin, ymin, xmax, ymax)
+target_sizes = torch.tensor([image.size[::-1]])
+results = image_processor.post_process_object_detection(
+    outputs, threshold=0.9, target_sizes=target_sizes
+)[0]
+
+from PIL import ImageDraw
+
+for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    draw = ImageDraw.Draw(image)
+    box = [round(i, 2) for i in box.tolist()]
+    print(
+        f"Detected {model.config.id2label[label.item()]} with confidence "
+        f"{round(score.item(), 3)} at location {box}"
+    )
+    xmin, ymin, xmax, ymax = box
+    draw.rectangle([xmin, ymin, xmax, ymax], outline="red", width=2)
+    image.show()
